a28c5cdc5ca2a8b7e9dde575e2426a4f9721510d
Huy Đỗ <huydo93.it@gmail.com>
2020-04-13 16:17:27 +0900

    gui_dev updated

--- form.py
+++ form.py
@@ -12,6 +12,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 # TEST GUI_TEST branch
 # Server - Client systems
 
+# ABCDEFGH, ABCHEFGH, ABCDEFGH
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
